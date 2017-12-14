d65014cd03a7e4dc8dab0f2bd3864e09760ddbf2
Peder Waern <pederwaern@hotmail.com>
2017-12-14 10:40:05 +0100

    changing..

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Hello {name}!</h3>" \
+    html = "<h3>Hello PEDER PEDER {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "Peder Waern World"), hostname=socket.gethostname(), visits=visits)
